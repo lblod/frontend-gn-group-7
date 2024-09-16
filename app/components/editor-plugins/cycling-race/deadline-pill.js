f4141f0850604343931753256da156c902e0cd9b
Oscar <lagartoverde97@gmail.com>
2024-09-16 14:02:59 +0200

    first version of the plugin

---
+++ app/components/editor-plugins/cycling-race/deadline-pill.js
@@ -0,0 +1,16 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { assert } from '@ember/debug';
+import { action } from '@ember/object';
+
+export default class WorshipPluginSearchModalComponent extends Component {
+  get skin() {
+    if (this.args.daysTillDeadline >= 100) {
+      return 'success';
+    } else if (this.args.daysTillDeadline >= 30) {
+      return 'warning';
+    } else {
+      return 'error';
+    }
+  }
+}
